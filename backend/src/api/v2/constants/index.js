fc52b739f6bdc0e6f084b343af748e002ddc665e
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-25 20:00:30 +0700

    modify constants file

--- backend/src/api/v2/constants/index.js
+++ backend/src/api/v2/constants/index.js
@@ -9,14 +9,12 @@ const Constants = {
     INTERNAL_SERVER_ERROR: 500,
 
     // Code from local app
-    CONNECTION_TIMEOUT: 'CONNECTION_TIMEOUT',
-    INTERNAL_SERVER: 'INTERNAL_SERVER',
-    UNKNOWN_NETWORK: 'UNKNOWN_NETWORK',
+    CONNECTION_TIMEOUT: "CONNECTION_TIMEOUT",
+    INTERNAL_SERVER: "INTERNAL_SERVER",
+    UNKNOWN_NETWORK: "UNKNOWN_NETWORK",
   },
 
-  ApiPath: {
-    
-  },
+  ApiPath: {},
 
   /**
    * Styles for app.
@@ -29,40 +27,48 @@ const Constants = {
     // =====================================================================
     // Common color
     // =====================================================================
-    PRIMARY_COLOR: '#15CDD3',
-    PRIMARY_DARK_COLOR: 'rgb(13, 142, 147)',
-    BLACK_COLOR: '#000000',
-    BLUE_COLOR: '#3b82f6',
-    BRIGHT_OCEAN_BLUE_COLOR: '#1F5DC6',
-    OCEAN_BLUE_COLOR: '#174291',
-    LIGHT_BLUE_COLOR: '#12B7BC',
-    GRAY_COLOR: '#808080',
-    LIGHT_GRAY_COLOR: '#e5e7eb',
-    GREEN_COLOR: '#008000',
-    RED_COLOR: '#dc2626',
-    WHITE_COLOR: '#FFFFFF',
+    PRIMARY_COLOR: "#15CDD3",
+    PRIMARY_DARK_COLOR: "rgb(13, 142, 147)",
+    BLACK_COLOR: "#000000",
+    BLUE_COLOR: "#3b82f6",
+    BRIGHT_OCEAN_BLUE_COLOR: "#1F5DC6",
+    OCEAN_BLUE_COLOR: "#174291",
+    LIGHT_BLUE_COLOR: "#12B7BC",
+    GRAY_COLOR: "#808080",
+    LIGHT_GRAY_COLOR: "#e5e7eb",
+    GREEN_COLOR: "#008000",
+    RED_COLOR: "#dc2626",
+    WHITE_COLOR: "#FFFFFF",
 
     // New - Analysis - Processing - Processed - Cancelled - Close
-    STATUS_COLOR: ['#27AE60', '#FEC600', '#24EBC7', '#00AFF0', '#D3D3D3', '#CED4DA'],
+    STATUS_COLOR: [
+      "#27AE60",
+      "#FEC600",
+      "#24EBC7",
+      "#00AFF0",
+      "#D3D3D3",
+      "#CED4DA",
+    ],
 
     // =====================================================================
     // Console log style
     // Color refer at: https://www.w3schools.com/w3css/w3css_colors.asp
     // =====================================================================
-    CONSOLE_LOG_DONE_ERROR: 'border: 2px solid #F44336; color: #000000', // Red
-    CONSOLE_LOG_DONE_SUCCESS: 'border: 2px solid #4CAF50; color: #000000', // Green
-    CONSOLE_LOG_ERROR: 'background: #F44336; color: #FFFFFF', // Red
-    CONSOLE_LOG_NOTICE: 'background: #FF9800; color: #000000; font-size: x-large', // Orange
-    CONSOLE_LOG_PREPARE: 'border: 2px solid #2196F3; color: #000000', // Blue
-    CONSOLE_LOG_START: 'background: #2196F3; color: #FFFFFF', // Blue
-    CONSOLE_LOG_SUCCESS: 'background: #4CAF50; color: #FFFFFF', // Green
+    CONSOLE_LOG_DONE_ERROR: "border: 2px solid #F44336; color: #000000", // Red
+    CONSOLE_LOG_DONE_SUCCESS: "border: 2px solid #4CAF50; color: #000000", // Green
+    CONSOLE_LOG_ERROR: "background: #F44336; color: #FFFFFF", // Red
+    CONSOLE_LOG_NOTICE:
+      "background: #FF9800; color: #000000; font-size: x-large", // Orange
+    CONSOLE_LOG_PREPARE: "border: 2px solid #2196F3; color: #000000", // Blue
+    CONSOLE_LOG_START: "background: #2196F3; color: #FFFFFF", // Blue
+    CONSOLE_LOG_SUCCESS: "background: #4CAF50; color: #FFFFFF", // Green
 
     // =====================================================================
     // Common size
     // =====================================================================
-    AVATAR_SIZE: '80px',
-    DEFAULT_FONTSIZE: '16px',
-    DEFAULT_SPACING: '24px',
+    AVATAR_SIZE: "80px",
+    DEFAULT_FONTSIZE: "16px",
+    DEFAULT_SPACING: "24px",
 
     // =====================================================================
     // Font size
@@ -82,45 +88,124 @@ const Constants = {
    * Regex Expression
    */
   RegExp: {
-    /** https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript */
-    EMAIL_ADDRESS: new RegExp(
-      `/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@` +
-        `((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/`,
+    EMAIL_ADDRESS: new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+    PASSWORD: new RegExp(
+      /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/
     ),
-    NEW_EMAIL_ADDRESS: new RegExp(
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    PHONE_NUMBER: new RegExp(
+      /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
     ),
-    /** https://gist.github.com/HarishChaudhari/0dd5514ce430991a1b1b8fa04e8b72a4 */
-    PASSWORD: new RegExp(`/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/`),
-    // PHONE_NUMBER: new RegExp(`/^(09|01|08|03|07|05[0-9])+([0-9]{8,9})$/`),
-    // PHONE_NUMBER: new RegExp(`/^0(1\d{9}|9\d{8})$/`),
-    PHONE_NUMBER: new RegExp(/^(?:0)?([1|3|5|7|8|9]{1})?([0-9]{8})$/),
   },
 
   /**
    * Storage keys
    */
-  StorageKeys: {
-    ACCESS_TOKEN: 'ACCESS_TOKEN',
-    USER_INFO: 'USER_INFO',
-    CONFIG_FOLDABLE: 'FOLDABLE',
-    CONFIG_SIDEBAR_SHOW: 'SIDEBAR_SHOW',
-    LANGUAGE: 'LANGUAGE',
+  StorageKeys: {},
+
+  String: {
+    Message: {
+      REQUIRED: (m = "") => `bạn phải nhập ${m}`,
+      MAX_LENGTH: (m = "") => `${m} quá dài`,
+      MIN_LENGTH: (m = "") => `${m} quá ngắn`,
+      UNIQUE: (m = "") => `${m} bị trùng`,
+      MATCH: (m = "") => `${m} không hợp lệ`,
+    },
+    Organization: {
+      NAME: "tên tổ chức",
+      CODE: "mã tổ chức",
+      EMAIL_ADDRESS: "địa chỉ email của tổ chức",
+      PHONE_NUMBER: "số điện thoại của tổ chức",
+    },
+    Type: {
+      NAME: "tên loại văn bản",
+      NOTATION: "ký hiệu loại văn bản",
+      DESCRIPTION: "mô tả loại văn bản",
+      COLOR: "màu loại văn bản",
+    },
+    Security: {
+      NAME: "tên độ mật",
+      DESCRIPTION: "mô tả độ mật",
+      COLOR: "màu độ mật",
+    },
+    Right: {
+      NAME: "tên quyền",
+      CREATE_OFFICIAL_DISPATCH: "quyền tạo mới công văn",
+      UPDATE_OFFICIAL_DISPATCH: "quyền cập nhật thông tin công văn",
+      DELETE_OFFICIAL_DISPATCH: "quyền xóa công văn",
+      APPROVE_OFFICIAL_DISPATCH: "quyền duyệt công văn",
+      CREATE_OFFICER: "quyền tạo mới cán bộ",
+      UPDATE_OFFICER: "quyền cập nhật thông tin cán bộ",
+      DELETE_OFFICER: "quyền xóa thông tin cán bộ",
+      CREATE_CATEGORIES: "quyền tạo mới danh mục",
+      UPDATE_CATEGORIES: "quyền cập nhật danh mục",
+      DELETE_CATEGORIES: "quyền xóa danh mục",
+      SCOPE: "phạm vi của quyền",
+    },
+    Status: {
+      NAME: "tên trạng thái công văn",
+      DESCRIPTION: "mô tả trạng thái công văn",
+      COLOR: "màu trạng thái công văn",
+    },
+    OfficerStatus: {
+      NAME: "tên trạng thái cán bộ",
+      DESCRIPTION: "mô tả trạng thái cán bộ",
+      COLOR: "màu trạng thái cán bộ",
+    },
+    Officer: {
+      CODE: "",
+      POSITION: "",
+      FIRST_NAME: "",
+      LAST_NAME: "",
+      EMAIL_ADDRESS: "",
+      PHONE_NUMBER: "",
+      PASSWORD: {
+        VALUE: "",
+        TIME: "",
+      },
+      ORGAN: "",
+      FILE: {
+        NAME: "",
+        PATH: "",
+        TYPE: "",
+        SIZE: "",
+      },
+      STATUS: "",
+      RIGHT: "",
+    },
+    IOD: {
+      CODE: "số văn bản",
+      ISSUED_DATE: "ngày văn bản",
+      SUBJECT: "trích yếu",
+      TYPE: "loại văn bản",
+      LANGUAGE: "ngôn ngữ văn bản",
+      PAGE_AMOUNT: "số trang",
+      DESCRIPTION: "mô tả",
+      SIGNER_INFO_POSITION: "chức vụ người ký văn bản",
+      SIGNER_INFO_NAME: "họ tên người ký văn bản",
+      DUE_DATE: "hạn giải quyết",
+      ARRIVAL_NUMBER: "số đến của văn bản",
+      ARRIVAL_DATE: "ngày đến",
+      PRIORITY: "mức độ khẩn",
+      SECURITY: "mức độ mật",
+      ORGAN: "cơ quan ban hành",
+      APPROVER: "cán bộ phê duyệt",
+      HANDLER: "cán bộ xử lý",
+      TRACE_HEADER_LIST: {
+        OFFICER: "người xử lý",
+        COMMAND: "nội dung xử lý",
+        DATE: "ngày xử lý",
+        HEADER: "tiêu đề",
+        STATUS: "trạng thái",
+      },
+      FILE: {
+        NAME: "tên file",
+        PATH: "đường dẫn",
+        TYPE: "loại file",
+        SIZE: "kích thước",
+      },
+      IMPORTER: "cán bộ nhập",
+    },
   },
+};
 
-  MomentWeekDay: {
-    Sunday: 0,
-    Monday: 1,
-    Tuesday: 2,
-    Wednesday: 3,
-    Thursday: 4,
-    Friday: 5,
-    Saturday: 6,
-  },
-
-  DefaultLanguage: 'vi',
-
-  COCCOC_BRAND_NAME: 'CocCoc',
-}
-
-module.exports = Constants
+module.exports = Constants;
