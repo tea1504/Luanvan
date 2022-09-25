96331ee6606a8e78f38dc0fb54c32797b9e4b571
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-25 10:50:46 +0700

    create priority model

---
+++ backend/src/api/v2/models/priority.model.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+var mongoose = require("mongoose");
+var databaseConfig = require("../../../config/database.config");
+
+mongoose.connect(databaseConfig.v2.path);
+
+const prioritySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+      required: false,
+      maxLength: 1000,
+    },
+    color: {
+      type: String,
+      required: true,
+      minLength: 7,
+      maxLength: 7,
+      default: "#7ed6df",
+    },
+  },
+  { collection: "priorities", timestamps: true }
+);
+
+module.exports = mongoose.model("priorities", prioritySchema);
