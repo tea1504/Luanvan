35ed13e62f5cb932cbd0bdd985bf3967c3b74f83
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-25 11:46:45 +0700

    modify migrate file

--- backend/src/api/v2/models/migrate.js
+++ backend/src/api/v2/models/migrate.js
@@ -4,6 +4,9 @@ const _type = require("./type.model");
 const _language = require("./language.model");
 const _security = require("./security.model");
 const _priority = require("./priority.model");
+const _right = require("./right.model");
+const _officerStatus = require("./officerStatus.model");
+const _organization = require("./organization.model");
 
 async function start() {
   console.log("Preparing ...");
@@ -44,6 +47,47 @@ async function start() {
       name: "priority 1",
     });
   }
+
+  console.log("Migrating Right");
+  {
+    await _right.deleteMany();
+    await _right.create({
+      _id: "right0000001",
+      name: "right 1",
+    });
+  }
+
+  console.log("Migrating Officer Status");
+  {
+    await _officerStatus.deleteMany();
+    await _officerStatus.create({
+      _id: "officerSta01",
+      name: "status 1",
+    });
+  }
+
+  console.log("Migrating Organization");
+  {
+    await _organization.deleteMany();
+    await _organization.create({
+      _id: "organ0000001",
+      name: "organization 1",
+      code: "1",
+      email: "o1.@gmail.com",
+      phoneNumber: "0939259664",
+    });
+    await _organization.create({
+      _id: "organ0000002",
+      name: "organization 2",
+      code: "2",
+      email: "o2.@gmail.com",
+      phoneNumber: "0939259665",
+      organ: "organ0000001",
+    });
+  }
+
+  var res = await _organization.find({ organ: { $eq: "organ0000001" } });
+  console.log(res);
 
   console.log("Done");
 }
