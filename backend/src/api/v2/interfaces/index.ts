18f195a4b4be8a7ce2dab6c0477a5fb602574654
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-27 22:22:59 +0700

    add interface TypeModel

--- backend/src/api/v2/interfaces/index.ts
+++ backend/src/api/v2/interfaces/index.ts
@@ -1,5 +1,17 @@
 export interface ResponseResult {
-  code: Number,
-  message: String,
-  data?: any
+  code: Number;
+  message: String;
+  data?: any;
+}
+
+export interface TypeModel {
+  _id?: String;
+  name: String;
+  notation: String;
+  description?: String;
+  color?: String;
+  deleted?: Boolean;
+  createdAt?: String;
+  updatedAt?: String;
+  __v: Number;
 }
