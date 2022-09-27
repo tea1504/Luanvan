1cf922cccbbaa3b3887a1788c422b6c9effc1158
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-27 22:23:44 +0700

    Add type router

--- backend/src/api/v2/routers/index.js
+++ backend/src/api/v2/routers/index.js
@@ -5,9 +5,10 @@ const officerRouter = require("./officer.router");
 const typeRouter = require("./type.router");
 const route = express.Router();
 const authMiddleware = require("./../middlewares/auth.middleware");
+const adminMiddleware = require("./../middlewares/admin.middleware");
 
 route.use(Constants.ApiPath.Auth.ROOT, authRouter);
 route.use(Constants.ApiPath.Officer.ROOT, authMiddleware, officerRouter);
-route.use(Constants.ApiPath.Type.ROOT, authMiddleware, typeRouter);
+route.use(Constants.ApiPath.Type.ROOT, authMiddleware, adminMiddleware, typeRouter);
 
 module.exports = route;
