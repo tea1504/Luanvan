e26f970992ad977c08588d932a93f0120b96c18f
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-09-29 15:22:23 +0700

    add type screens

--- frontend/src/constants/screens.js
+++ frontend/src/constants/screens.js
@@ -5,10 +5,16 @@ const Screens = {
   HOME: '/',
   CREATE_USER: '/user/create',
   UPDATE_USER: '/user/update',
+  // Book
   BOOK: '/book',
   BOOK_DETAIL: (id = ':id') => '/book/detail/' + id,
   BOOK_CREATE: '/book/create',
   BOOK_UPDATE: (id = ':id') => '/book/update/' + id,
+  // Type
+  TYPE: '/types',
+  TYPE_DETAIL: (id = ':id') => '/types/detail/' + id,
+  TYPE_CREATE: '/types/create',
+  TYPE_UPDATE: (id = ':id') => '/types/update/' + id,
   // =========================================================================
   // Public
   // =========================================================================
