b993f77d65a0bff85441bd987119864b7f329698
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-10-04 21:16:25 +0700

    add Post userInfo

--- backend/src/api/v2/routers/auth.router.js
+++ backend/src/api/v2/routers/auth.router.js
@@ -3,12 +3,32 @@ const Constants = require("../constants");
 const authController = require("./../controllers/auth.controller");
 const route = express.Router();
 const authMiddleware = require("./../middlewares/auth.middleware");
+const multer = require("multer");
+const path = require("path");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join("public/avatars/"));
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
 
 route.post(Constants.ApiPath.Auth.LOGIN, authController.login);
 route.get(
   Constants.ApiPath.Auth.GET_INFO,
   authMiddleware,
   authController.getInfo
+);
+route.post(
+  Constants.ApiPath.Auth.PUT_INFO,
+  authMiddleware,
+  upload.single("avatar"),
+  authController.putInfo
 );
 
 module.exports = route;
