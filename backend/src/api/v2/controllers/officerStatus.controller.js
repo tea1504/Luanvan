fa49e61607f7772da46247157c79c996a89f1d47
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-10-06 16:26:09 +0700

    add postOfficerStatuses

--- backend/src/api/v2/controllers/officerStatus.controller.js
+++ backend/src/api/v2/controllers/officerStatus.controller.js
@@ -1,5 +1,6 @@
 const Constants = require("../constants");
 const officerStatusService = require("./../services/officerStatus.service");
+var fs = require("fs");
 
 var officerStatusController = {
   /**
@@ -29,7 +30,9 @@ var officerStatusController = {
     try {
       const { id } = req.params;
       list = id.split(".");
-      const result = await officerStatusService.getOfficerStatus(list[list.length - 1]);
+      const result = await officerStatusService.getOfficerStatus(
+        list[list.length - 1]
+      );
       return res.status(result.status).json(result);
     } catch (error) {
       return next(error);
@@ -65,6 +68,31 @@ var officerStatusController = {
    * @param {import("express").Response} res
    * @param {import("express").RequestHandler} next
    */
+  postOfficerStatuses: async (req, res, next) => {
+    try {
+      const file = req.file;
+      const { text, title } = req.body;
+      if (file) {
+        const data = fs.readFileSync(file.path);
+        const index = data.toString().indexOf("\n");
+        var t = data.toString();
+        if (title === "true") t = t.slice(index + 1);
+        const result = await officerStatusService.postOfficerStatuses(t);
+        fs.unlinkSync(file.path);
+        return res.status(result.status).json(result);
+      } else {
+        const result = await officerStatusService.postOfficerStatuses(text);
+        return res.status(result.status).json(result);
+      }
+    } catch (error) {
+      return next(error);
+    }
+  },
+  /**
+   * @param {import("express").Request} req
+   * @param {import("express").Response} res
+   * @param {import("express").RequestHandler} next
+   */
   putOfficerStatus: async (req, res, next) => {
     try {
       const { id } = req.params;
@@ -77,11 +105,14 @@ var officerStatusController = {
             Constants.String.OfficerStatus.NAME
           ),
         });
-      const result = await officerStatusService.putOfficerStatus(list[list.length - 1], {
-        name,
-        description,
-        color,
-      });
+      const result = await officerStatusService.putOfficerStatus(
+        list[list.length - 1],
+        {
+          name,
+          description,
+          color,
+        }
+      );
       return res.status(result.status).json(result);
     } catch (error) {
       return next(error);
