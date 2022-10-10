656283d7610cbd19a014d61d1c45a82d55f89cb6
Trần Văn Hòa <tranvanhoa15042000@gmail.com>
2022-10-10 11:08:42 +0700

    change code to scope

--- frontend/src/components/AppContent.js
+++ frontend/src/components/AppContent.js
@@ -27,7 +27,7 @@ const AppContent = () => {
                   name={route.name}
                   element={
                     localStorage.getItem(Constants.StorageKeys.ACCESS_TOKEN) !== null ? (
-                      !route.role || route.role.includes(user ? user.right.code : -1) ? (
+                      !route.role || route.role.includes(user ? user.right.scope : -1) ? (
                         <route.element />
                       ) : (
                         <Navigate to="/403" />
